958418ef07c720626f9658cc2eba292b07e68507
professormr5 <ravansr18@gmail.com>
2025-06-23 15:11:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
         clientId: "a568b55af1d940aca52ea8fe02f0d93b",
         clientSecret: "e8199f4024fe49c5b22ea9a3dd0c4789"
     },
-    botToken: "",
+    botToken: "MTI4NDQ3ODE0NDI4NzM0MjY5Mw.GR3Mu7.6pkFRYLY4s089vtHUqOpH_svV7Sj-LGO0jk4Kk",
     embedColor: "#0061ff"
 };
